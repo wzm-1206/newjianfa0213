21a6a019b535ffa82759b08e7ddc07ca7a579088
wzm-1206 <773552770@qq.com>
2020-07-14 11:38:28 +0800

    dugujiujian

---
+++ src/main/java/com/atguigu/huashan/Dugujiujian.java
@@ -0,0 +1,17 @@
+package com.atguigu.huashan;
+
+/**
+ * @author wzmstart
+ * @create 2020-07-14 11:12
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("dugujiujian9999999999999999");
+        System.out.println("dugujiujian9999999999999999");
+        System.out.println("dugujiujian9999999999999999");
+        System.out.println("dugujiujian9999999999999999");
+
+        //红色 未追踪代码 绿色 暂存区
+
+    }
+}
