b2bba539957177d2efdac4b284c8b2ce050af156
wzm-1206 <773552770@qq.com>
2020-07-14 14:39:28 +0800

    xixing

---
+++ src/main/java/com/atguigu/riyue/Xixingdafa.java
@@ -0,0 +1,11 @@
+package com.atguigu.riyue;
+
+/**
+ * @author wzmstart
+ * @create 2020-07-14 14:36
+ */
+public class Xixingdafa {
+    public static void main(String[] args) {
+        System.out.println("吸星大法好..........");
+    }
+}
